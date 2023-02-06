7bfea2dda2a0711a26f57cd826c2ba78f930d1a2
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-02-06 22:59:43 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { onSearch } from './search';
+
+export const refs = {
+    searchForm: document.querySelector('#search-box'),
+    countriesList: document.querySelector('.country-list'),
+    countryCard: document.querySelector('.country-info'),
+};
+
 
 const DEBOUNCE_DELAY = 300;
+
+refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
